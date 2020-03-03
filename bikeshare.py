aba029629dc8150d2c7b8f28215c2982f322d33f
Michael Ibele <michael.ibele@zf.com>
2020-03-03 11:05:13 +0100

    docs: changed some descirptions of subtasks to provide more information

--- bikeshare.py
+++ bikeshare.py
@@ -194,7 +194,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users. Like gender, age or the type of the user (private, commercial) """
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
